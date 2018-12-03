a39374dd91e6c2e9b6d31f11ac3e33af67043a86
Jeremy Meredith <jsmeredith@gmail.com>
2018-12-03 10:54:48 -0700

    Updating formatting and API name string for java Drive Activity v2 quickstart.

--- drive/activity-v2/quickstart/src/main/java/DriveActivityQuickstart.java
+++ drive/activity-v2/quickstart/src/main/java/DriveActivityQuickstart.java
@@ -18,167 +18,164 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DriveActivityQuickstart {
-  /** Application name. */
-  private static final String APPLICATION_NAME = "G Suite Activity API Java Quickstart";
+    /** Application name. */
+    private static final String APPLICATION_NAME = "Drive Activity API Java Quickstart";
 
-  /** Directory to store authorization tokens for this application. */
-  private static final java.io.File DATA_STORE_DIR = new java.io.File("tokens");
+    /** Directory to store authorization tokens for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File("tokens");
 
-  /** Global instance of the {@link FileDataStoreFactory}. */
-  private static FileDataStoreFactory DATA_STORE_FACTORY;
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
 
-  /** Global instance of the JSON factory. */
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-  /** Global instance of the HTTP transport. */
-  private static HttpTransport HTTP_TRANSPORT;
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
 
-  /**
-   * Global instance of the scopes required by this quickstart.
-   *
-   * <p>If modifying these scopes, delete your previously saved credentials at
-   * ~/.credentials/driveactivity-java-quickstart
-   */
-  private static final List<String> SCOPES =
-      Arrays.asList(DriveActivityScopes.DRIVE_ACTIVITY_READONLY);
+    /**
+     * Global instance of the scopes required by this quickstart.
+     *
+     * <p>If modifying these scopes, delete your previously saved credentials at
+     * ~/.credentials/driveactivity-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(DriveActivityScopes.DRIVE_ACTIVITY_READONLY);
 
-  static {
-    try {
-      HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-      DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-    } catch (Throwable t) {
-      t.printStackTrace();
-      System.exit(1);
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
     }
-  }
 
-  /**
-   * Creates an authorized Credential object.
-   *
-   * @return an authorized Credential object.
-   * @throws IOException
-   */
-  public static Credential authorize() throws IOException {
-    // Load client secrets.
-    InputStream in = DriveActivityQuickstart.class.getResourceAsStream("/credentials.json");
-    GoogleClientSecrets clientSecrets =
-        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in = DriveActivityQuickstart.class.getResourceAsStream("/credentials.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-    // Build flow and trigger user authorization request.
-    GoogleAuthorizationCodeFlow flow =
-        new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-            .setDataStoreFactory(DATA_STORE_FACTORY)
-            .setAccessType("offline")
-            .build();
-    Credential credential =
-        new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-    System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-    return credential;
-  }
-
-  /**
-   * Build and return an authorized Drive Activity client service.
-   *
-   * @return an authorized DriveActivity client service
-   * @throws IOException
-   */
-  public static com.google.api.services.driveactivity.v2.DriveActivity getDriveActivityService()
-      throws IOException {
-    Credential credential = authorize();
-    return new com.google.api.services.driveactivity.v2.DriveActivity.Builder(
-            HTTP_TRANSPORT, JSON_FACTORY, credential)
-        .setApplicationName(APPLICATION_NAME)
-        .build();
-  }
-
-  public static void main(String[] args) throws IOException {
-    // Build a new authorized API client service.
-    com.google.api.services.driveactivity.v2.DriveActivity service = getDriveActivityService();
-
-    // Print the recent activity in your Google Drive.
-    QueryDriveActivityResponse result =
-        service
-            .activity()
-            .query(
-                new QueryDriveActivityRequest()
-                    .setPageSize(10)
-                    .setConsolidationStrategy(new ConsolidationStrategy().setLegacy(new Legacy())))
-            .execute();
-    List<DriveActivity> activities = result.getActivities();
-    if (activities == null || activities.size() == 0) {
-      System.out.println("No activity.");
-    } else {
-      System.out.println("Recent activity:");
-      for (DriveActivity activity : activities) {
-        String time = getTimeInfo(activity);
-        String action = getActionInfo(activity.getPrimaryActionDetail());
-        List<String> actors =
-            activity.getActors().stream()
-                .map(DriveActivityQuickstart::getActorInfo)
-                .collect(Collectors.toList());
-        List<String> targets =
-            activity.getTargets().stream()
-                .map(DriveActivityQuickstart::getTargetInfo)
-                .collect(Collectors.toList());
-        System.out.printf("%s: %s, %s, %s\n", time, truncated(actors), action, truncated(targets));
-      }
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential =
+                new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver())
+                        .authorize("user");
+        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
     }
-  }
 
-  private static String truncated(List<String> array) {
-    return truncated(array, 2);
-  }
+    /**
+     * Build and return an authorized Drive Activity client service.
+     *
+     * @return an authorized DriveActivity client service
+     * @throws IOException
+     */
+    public static com.google.api.services.driveactivity.v2.DriveActivity getDriveActivityService()
+            throws IOException {
+        Credential credential = authorize();
+        return new com.google.api.services.driveactivity.v2.DriveActivity.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
-  private static String truncated(List<String> array, int limit) {
-    String contents = array.stream().limit(limit).collect(Collectors.joining(", "));
-    String more = array.size() > limit ? ", ..." : "";
-    return "[" + contents + more + "]";
-  }
+    public static void main(String[] args) throws IOException {
+        // Build a new authorized API client service.
+        com.google.api.services.driveactivity.v2.DriveActivity service = getDriveActivityService();
 
-  private static String getTimeInfo(DriveActivity activity) {
-    if (activity.getTimestamp() != null) {
-      return activity.getTimestamp();
+        // Print the recent activity in your Google Drive.
+        QueryDriveActivityResponse result =
+                service.activity().query(new QueryDriveActivityRequest().setPageSize(10)).execute();
+        List<DriveActivity> activities = result.getActivities();
+        if (activities == null || activities.size() == 0) {
+            System.out.println("No activity.");
+        } else {
+            System.out.println("Recent activity:");
+            for (DriveActivity activity : activities) {
+                String time = getTimeInfo(activity);
+                String action = getActionInfo(activity.getPrimaryActionDetail());
+                List<String> actors =
+                        activity.getActors().stream()
+                                .map(DriveActivityQuickstart::getActorInfo)
+                                .collect(Collectors.toList());
+                List<String> targets =
+                        activity.getTargets().stream()
+                                .map(DriveActivityQuickstart::getTargetInfo)
+                                .collect(Collectors.toList());
+                System.out.printf(
+                        "%s: %s, %s, %s\n", time, truncated(actors), action, truncated(targets));
+            }
+        }
     }
-    if (activity.getTimeRange() != null) {
-      return activity.getTimeRange().getEndTime();
-    }
-    return "unknown";
-  }
 
-  private static String getActionInfo(ActionDetail actionDetail) {
-    return actionDetail.keySet().iterator().next();
-  }
+    private static String truncated(List<String> array) {
+        return truncated(array, 2);
+    }
 
-  private static String getUserInfo(User user) {
-    if (user.getKnownUser() != null) {
-      KnownUser knownUser = user.getKnownUser();
-      Boolean isMe = knownUser.getIsCurrentUser();
-      return (isMe != null && isMe) ? "people/me" : knownUser.getPersonName();
+    private static String truncated(List<String> array, int limit) {
+        String contents = array.stream().limit(limit).collect(Collectors.joining(", "));
+        String more = array.size() > limit ? ", ..." : "";
+        return "[" + contents + more + "]";
     }
-    return user.keySet().iterator().next();
-  }
 
-  private static String getActorInfo(Actor actor) {
-    if (actor.getUser() != null) {
-      return getUserInfo(actor.getUser());
+    private static String getTimeInfo(DriveActivity activity) {
+        if (activity.getTimestamp() != null) {
+            return activity.getTimestamp();
+        }
+        if (activity.getTimeRange() != null) {
+            return activity.getTimeRange().getEndTime();
+        }
+        return "unknown";
     }
-    return actor.keySet().iterator().next();
-  }
 
-  private static String getTargetInfo(Target target) {
-    if (target.getDriveItem() != null) {
-      return "driveItem:\"" + target.getDriveItem().getTitle() + "\"";
+    private static String getActionInfo(ActionDetail actionDetail) {
+        return actionDetail.keySet().iterator().next();
     }
-    if (target.getTeamDrive() != null) {
-      return "teamDrive:\"" + target.getTeamDrive().getTitle() + "\"";
+
+    private static String getUserInfo(User user) {
+        if (user.getKnownUser() != null) {
+            KnownUser knownUser = user.getKnownUser();
+            Boolean isMe = knownUser.getIsCurrentUser();
+            return (isMe != null && isMe) ? "people/me" : knownUser.getPersonName();
+        }
+        return user.keySet().iterator().next();
     }
-    if (target.getFileComment() != null) {
-      DriveItem parent = target.getFileComment().getParent();
-      if (parent != null) {
-        return "fileComment:\"" + parent.getTitle() + "\"";
-      }
-      return "fileComment:unknown";
+
+    private static String getActorInfo(Actor actor) {
+        if (actor.getUser() != null) {
+            return getUserInfo(actor.getUser());
+        }
+        return actor.keySet().iterator().next();
     }
-    return target.keySet().iterator().next();
-  }
+
+    private static String getTargetInfo(Target target) {
+        if (target.getDriveItem() != null) {
+            return "driveItem:\"" + target.getDriveItem().getTitle() + "\"";
+        }
+        if (target.getTeamDrive() != null) {
+            return "teamDrive:\"" + target.getTeamDrive().getTitle() + "\"";
+        }
+        if (target.getFileComment() != null) {
+            DriveItem parent = target.getFileComment().getParent();
+            if (parent != null) {
+                return "fileComment:\"" + parent.getTitle() + "\"";
+            }
+            return "fileComment:unknown";
+        }
+        return target.keySet().iterator().next();
+    }
 }
