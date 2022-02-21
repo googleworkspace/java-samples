a3cc78fda807be1d2bb34ab758c9b5f97d5f8418
sanjuktaghosh7 <sanjuktaghosh@google.com>
2022-02-21 19:37:04 +0530

    Update BaseTest.java

--- classroom/snippets/src/test/java/BaseTest.java
+++ classroom/snippets/src/test/java/BaseTest.java
@@ -1,5 +1,3 @@
-package com.google.classroom.snippets;
-
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
