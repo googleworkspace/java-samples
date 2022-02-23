d13cfc753479273666577498307bad8b06adca95
sanjuktaghosh7 <sanjuktaghosh@google.com>
2022-02-23 20:52:32 +0530

    classroom-api-snippets

---
+++ classroom/snippets/src/main/java/GetCourse.java
@@ -0,0 +1,71 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// [START classroom_get_course]
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.classroom.Classroom;
+import com.google.api.services.classroom.ClassroomScopes;
+import com.google.api.services.classroom.model.Course;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.IOException;
+import java.util.Collections;
+
+/* Class to demonstrate the use of Classroom Get Course API */
+public class GetCourse {
+    /**
+     * Retrieve a single course's metadata.
+     *
+     * @param courseId - Id of the course to return.
+     * @return a course
+     * @throws IOException - if credentials file not found.
+     */
+    public static Course getCourse(String courseId) throws IOException {
+        /* Load pre-authorized user credentials from the environment.
+           TODO(developer) - See https://developers.google.com/identity for
+            guides on implementing OAuth2 for your application. */
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(Collections.singleton(ClassroomScopes.CLASSROOM_COURSES));
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
+                credentials);
+
+        // Create the classroom API client
+        Classroom service = new Classroom.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Classroom samples")
+                .build();
+
+        Course course = null;
+        try {
+            course = service.courses().get(courseId).execute();
+            System.out.printf("Course '%s' found.\n", course.getName());
+        } catch (GoogleJsonResponseException e) {
+            // TODO(developer) - handle error appropriately
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 404) {
+                System.out.printf("Course with ID '%s' not found.\n", courseId);
+            } else {
+                throw e;
+            }
+        }
+        return course;
+    }
+}
+// [END classroom_get_course]
