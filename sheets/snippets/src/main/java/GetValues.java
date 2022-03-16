cb2bc169d90926b88f96c6430475fd39c1fa611b
Rajesh Mudaliyar <mudaliyar@google.com>
2022-03-16 09:07:11 -0700

    Sheets snippets1 (#213)
    * git-on-borg files of gmail-api-snippets
    
    * Update build.gradle
    
    test12
    
    * Update build.gradle
    
    * Spreadsheet snippets
    
    Co-authored-by: sanjuktaghosh7 <sanjuktaghosh@google.com>
    Co-authored-by: Manvendra-P-Singh <singhmanvendra@google.com>

---
+++ sheets/snippets/src/main/java/GetValues.java
@@ -0,0 +1,75 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// [START sheets_get_values]
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.IOException;
+import java.util.Collections;
+
+/* Class to demonstrate the use of Spreadsheet Get Values API */
+public class GetValues {
+    /**
+     * Returns a range of values from a spreadsheet.
+     *
+     * @param spreadsheetId - Id of the spreadsheet.
+     * @param range - Range of cells of the spreadsheet.
+     * @return Values in the range
+     * @throws IOException - if credentials file not found.
+     */
+    public static ValueRange getValues(String spreadsheetId, String range) throws IOException {
+        /* Load pre-authorized user credentials from the environment.
+           TODO(developer) - See https://developers.google.com/identity for
+            guides on implementing OAuth2 for your application. */
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
+                credentials);
+
+        // Create the sheets API client
+        Sheets service = new Sheets.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Sheets samples")
+                .build();
+
+        ValueRange result = null;
+        try {
+            // Gets the values of the cells in the specified range.
+            result = service.spreadsheets().values().get(spreadsheetId, range).execute();
+            int numRows = result.getValues() != null ? result.getValues().size() : 0;
+            System.out.printf("%d rows retrieved.", numRows);
+        } catch (GoogleJsonResponseException e) {
+            // TODO(developer) - handle error appropriately
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 404) {
+                System.out.printf("Spreadsheet not found with id '%s'.\n",spreadsheetId);
+            } else {
+                throw e;
+            }
+        }
+        return result;
+    }
+}
+// [END sheets_get_values]
