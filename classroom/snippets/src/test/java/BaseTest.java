aeac242af855580a81cdc070af4043f5b771fb41
sanjuktaghosh7 <sanjuktaghosh@google.com>
2022-05-22 17:57:10 +0530

    Update BaseTest.java

--- classroom/snippets/src/test/java/BaseTest.java
+++ classroom/snippets/src/test/java/BaseTest.java
@@ -28,7 +28,7 @@ public class BaseTest {
            TODO(developer) - See https://developers.google.com/identity for
             guides on implementing OAuth2 for your application. */
         GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-                .createScoped(Collections.singleton(ClassroomScopes.CLASSROOM_ROSTERS));
+                .createScoped(ClassroomScopes.CLASSROOM_ROSTERS);
         HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
                 credentials);
 
