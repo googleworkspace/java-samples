4eec0616549ecd59bed4045048e0cd753c1b8f57
Steven Bazyl <sbazyl@google.com>
2022-08-30 14:52:19 -0600

    fix: Add missing import

--- drive/snippets/drive_v2/src/test/java/TestListAppdata.java
+++ drive/snippets/drive_v2/src/test/java/TestListAppdata.java
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import static org.junit.Assert.assertNotEquals;
 import com.google.api.services.drive.model.FileList;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
