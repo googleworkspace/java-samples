c0b8c1bb9d2bead4ce3ea51618150e4b32c65182
Mahima Desetty <mahimadesetty@google.com>
2023-01-11 15:45:48 -0500

    fixing BaseTest.java

--- classroom/snippets/src/test/java/BaseTest.java
+++ classroom/snippets/src/test/java/BaseTest.java
@@ -47,11 +47,10 @@ public class BaseTest {
         Collections.singletonList(ClassroomScopes.CLASSROOM_COURSES);
 
     // Create the classroom API client
-    ClassroomCredentials classroomCredentials = new ClassroomCredentials();
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
     Classroom service = new Classroom.Builder(HTTP_TRANSPORT,
         GsonFactory.getDefaultInstance(),
-        classroomCredentials.getCredentials(HTTP_TRANSPORT, SCOPES))
+        ClassroomCredentials.getCredentials(HTTP_TRANSPORT, SCOPES))
         .setApplicationName("Classroom samples")
         .build();
 
