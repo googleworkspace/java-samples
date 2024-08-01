82072fa0f5746a914ec5ee65f40630cd5cb18f8e
pierrick <pierrick@google.com>
2024-08-01 18:17:30 +0000

    Fix Google Style

--- chat/quickstart/src/main/java/ChatQuickstart.java
+++ chat/quickstart/src/main/java/ChatQuickstart.java
@@ -14,15 +14,6 @@
 
 // [START chat_quickstart]
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -41,24 +32,28 @@ import com.google.chat.v1.ChatServiceSettings;
 import com.google.chat.v1.ListSpacesRequest;
 import com.google.chat.v1.Space;
 import com.google.protobuf.util.JsonFormat;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 /* class to demonstrate use of Google Chat API spaces list API */
 public class ChatQuickstart {
-  /**
-   * Directory to store authorization tokens for this application.
-   */
+  /** Directory to store authorization tokens for this application. */
   private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
   /**
-   * Global instance of the scopes required by this quickstart.
-   * If modifying these scopes, delete your previously saved tokens/ folder.
+   * Global instance of the scopes required by this quickstart. If modifying these scopes, delete
+   * your previously saved tokens/ folder.
    */
-  private static final List<String> SCOPES = Collections
-      .singletonList("https://www.googleapis.com/auth/chat.spaces.readonly");
+  private static final List<String> SCOPES =
+      Collections.singletonList("https://www.googleapis.com/auth/chat.spaces.readonly");
 
-  /**
-   * Global instance of the JSON factory.
-   */
+  /** Global instance of the JSON factory. */
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
   private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
@@ -72,42 +67,39 @@ public class ChatQuickstart {
   private static Credentials getCredentials() throws Exception {
     // Load client secrets.
     InputStream credentialsFileInputStream =
-      ChatQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        ChatQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
     if (credentialsFileInputStream == null) {
       throw new FileNotFoundException("Credentials file resource not found.");
     }
-    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-        JSON_FACTORY, new InputStreamReader(credentialsFileInputStream));
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(credentialsFileInputStream));
 
     // Set up authorization code flow.
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-        GoogleNetHttpTransport.newTrustedTransport(),
-        JSON_FACTORY,
-        clientSecrets,
-        SCOPES)
-      // Set these two options to generate refresh token alongside access token.
-      .setDataStoreFactory(
-        new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
-      .setAccessType("offline")
-      .build();
+    GoogleAuthorizationCodeFlow flow =
+        new GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, SCOPES)
+            // Set these two options to generate refresh token alongside access token.
+            .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build();
 
     // Authorize.
-    Credential credential = new AuthorizationCodeInstalledApp(
-        flow, new LocalServerReceiver()).authorize("user");
+    Credential credential =
+        new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
 
     // Build and return an authorized Credential object
-    AccessToken accessToken = new AccessToken(
-        credential.getAccessToken(),
-        new Date(
-          // put the actual expiry date of access token here
-          System.currentTimeMillis()));
-    return UserCredentials
-      .newBuilder()
-      .setAccessToken(accessToken)
-      .setRefreshToken(credential.getRefreshToken())
-      .setClientId(clientSecrets.getInstalled().getClientId())
-      .setClientSecret(clientSecrets.getInstalled().getClientSecret())
-      .build();
+    AccessToken accessToken =
+        new AccessToken(
+            credential.getAccessToken(),
+            new Date(
+                // put the actual expiry date of access token here
+                System.currentTimeMillis()));
+    return UserCredentials.newBuilder()
+        .setAccessToken(accessToken)
+        .setRefreshToken(credential.getRefreshToken())
+        .setClientId(clientSecrets.getInstalled().getClientId())
+        .setClientSecret(clientSecrets.getInstalled().getClientSecret())
+        .build();
   }
 
   public static void main(String... args) throws Exception {
@@ -116,24 +108,20 @@ public class ChatQuickstart {
 
     // Create the ChatServiceSettings with the credentials
     ChatServiceSettings chatServiceSettings =
-      ChatServiceSettings
-        .newBuilder()
-        .setCredentialsProvider(
-          FixedCredentialsProvider.create(credentials))
-        .build();
+        ChatServiceSettings.newBuilder()
+            .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+            .build();
 
-    try (ChatServiceClient chatServiceClient =
-        ChatServiceClient.create(chatServiceSettings)) {
+    try (ChatServiceClient chatServiceClient = ChatServiceClient.create(chatServiceSettings)) {
       ListSpacesRequest request =
-        ListSpacesRequest.newBuilder()
-          // Filter spaces by space type (SPACE or GROUP_CHAT or
-          // DIRECT_MESSAGE).
-          .setFilter("spaceType = \"SPACE\"")
-          .build();
+          ListSpacesRequest.newBuilder()
+              // Filter spaces by space type (SPACE or GROUP_CHAT or
+              // DIRECT_MESSAGE).
+              .setFilter("spaceType = \"SPACE\"")
+              .build();
 
       // Iterate over results and resolve additional pages automatically.
-      for (Space response :
-          chatServiceClient.listSpaces(request).iterateAll()) {
+      for (Space response : chatServiceClient.listSpaces(request).iterateAll()) {
         System.out.println(JsonFormat.printer().print(response));
       }
     }
