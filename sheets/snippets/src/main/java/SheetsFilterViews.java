cba13c95f6b985f4bd074c18278f3d0b0a81ad45
Ken Cenerelli <ken_cenerelli@hotmail.com>
2026-02-12 16:12:35 -0800

    Fix comment formatting in SheetsFilterViews.java

--- sheets/snippets/src/main/java/SheetsFilterViews.java
+++ sheets/snippets/src/main/java/SheetsFilterViews.java
@@ -1,4 +1,4 @@
-"""
+/ *
 Copyright 2022 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,9 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
+* /
 
-# [START sheets_filter_views]
+// [START sheets_filter_views]
 
 /*
  * Dependencies (Maven):
@@ -37,9 +37,7 @@ import java.util.*;
 public class SheetsFilterViews {
 
     public static void main(String... args) {
-        // [START sheets_filter_views]
         filterViews("1CM29gwKIzeXsAppeNwrc8lbYaVMmUclprLuLYuHog4k");
-        // [END sheets_filter_views]
     }
 
     public static void filterViews(String spreadsheetId) {
@@ -149,4 +147,4 @@ public class SheetsFilterViews {
     }
 }
 
-# [END sheets_filter_views]
+// [END sheets_filter_views]
